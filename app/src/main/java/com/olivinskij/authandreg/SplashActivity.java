ab11bc1c2eec654fed8a54ed07fd1788ff75d76a
Olivinskiy <maxim.olivinskij@gmail.com>
2024-12-11 10:13:54 +0200

    test commit

--- app/src/main/java/com/olivinskij/authandreg/SplashActivity.java
+++ app/src/main/java/com/olivinskij/authandreg/SplashActivity.java
@@ -36,6 +36,6 @@ public class SplashActivity extends AppCompatActivity {
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                 finish();
             }
-        }, 5000);
+        }, 3000);
     }
 }
